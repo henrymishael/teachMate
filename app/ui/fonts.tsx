d6dfad50703649f4257bdf3b6c03746ec600c335
henrymishael <henrymishael.hm@gmail.com>
2024-01-25 11:09:08 +0100

    layout added

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,15 @@
+import { Inter, Cabin, Lusitana } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+export const cabin = Cabin({
+  subsets: ["latin"],
+});
---
+++ app/ui/teachMate-logo.tsx
@@ -0,0 +1,14 @@
+import { lusitana } from "@/app/ui/fonts";
+import logo from "../../public/logo2.png";
+import Image from "next/image";
+
+export default function TeachMateLogo() {
+  return (
+    <div
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+    >
+      {/* <Image src={logo} alt='Teach Mate Logo' height={80} /> */}
+      <p className='text-[44px]'>TeachMateAI</p>
+    </div>
+  );
+}
